aedc49febc46e3d7c2b416a5b159e257f706d0cd
2Math0 <toto.mm50.tm@gmail.com>
2023-01-13 13:08:48 +0200

    improve the hierarchy of files and add some pre-coded resources

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:my_fate/main.dart';
+import 'package:my_fate/app/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
