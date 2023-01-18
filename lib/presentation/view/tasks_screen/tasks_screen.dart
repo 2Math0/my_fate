6532d4e064efab152e224a625e774af8944b6deb
2Math0 <toto.mm50.tm@gmail.com>
2023-01-18 10:07:31 +0200

    change to silver widgets

--- lib/presentation/view/tasks_screen/tasks_screen.dart
+++ lib/presentation/view/tasks_screen/tasks_screen.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:my_fate/presentation/resources/assets_manager.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:my_fate/presentation/resources/colors_manager.dart';
 import 'package:my_fate/presentation/resources/strings_manager.dart';
 import 'package:my_fate/presentation/resources/text_styles_manager.dart';
 import 'package:my_fate/presentation/resources/values_manager.dart';
-import 'package:my_fate/presentation/view/tasks_screen/componentes/date_picker.dart';
+
+import '../../resources/assets_manager.dart';
+import 'components/date_picker.dart';
 
 class TaskScreen extends StatelessWidget {
   const TaskScreen({Key? key}) : super(key: key);
@@ -16,40 +17,37 @@ class TaskScreen extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: AppColors.kBlack,
-      appBar: TaskAppBar(
-        title: AppStrings.personalTasks,
-        description: AppStrings.taskDescription,
-        size: size,
-      ),
-      body: Container(
-        height: size.height,
-        padding: const EdgeInsets.all(AppPadding.p20),
-        decoration: const BoxDecoration(
-            color: AppColors.kWhite,
-            borderRadius: BorderRadius.vertical(
-                top: Radius.circular(AppCircularRadius.cr24))),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const DatePicker(),
-            SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Text(AppStrings.taskDescription),
-                  Text(AppStrings.taskDescription),
-                  Text(AppStrings.taskDescription),
-                ],
-              ),
-            )
-          ],
+      body: CustomScrollView(slivers: [
+        TaskAppBar(
+          title: AppStrings.personalTasks,
+          description: AppStrings.taskDescription,
+          size: size,
         ),
-      ),
+        SliverToBoxAdapter(
+          child: Container(
+            height: size.height,
+            padding: const EdgeInsets.all(AppPadding.p20),
+            decoration: const BoxDecoration(
+                color: AppColors.kWhite,
+                borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(AppCircularRadius.cr24))),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: const [
+                DatePicker(),
+                Text(AppStrings.taskDescription),
+                Text(AppStrings.taskDescription),
+                Text(AppStrings.taskDescription)
+              ],
+            ),
+          ),
+        ),
+      ]),
     );
   }
 }
 
-class TaskAppBar extends StatelessWidget implements PreferredSizeWidget {
+class TaskAppBar extends StatelessWidget {
   final String title;
   final String description;
   final Size size;
@@ -61,50 +59,45 @@ class TaskAppBar extends StatelessWidget implements PreferredSizeWidget {
     required this.size,
   }) : super(key: key);
 
-  @override
   Size get preferredSize => Size.fromHeight(size.height * 0.14);
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      leading: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          const SizedBox(height: AppSize.s4),
-          IconButton(
-              onPressed: () => Navigator.pop(context),
-              icon: SvgPicture.asset(
-                AppIcons.leftArrow,
-                color: AppColors.kWhite,
-                height: AppSize.s12,
-              ))
-        ],
-      ),
-      toolbarHeight: preferredSize.height,
-      backgroundColor: AppColors.kBlack,
-      systemOverlayStyle: const SystemUiOverlayStyle(
-          statusBarColor: Colors.transparent,
-          statusBarBrightness: Brightness.light,
-          statusBarIconBrightness: Brightness.light),
-      flexibleSpace: Padding(
-        padding: EdgeInsets.only(
-            left: AppMargin.m48,
-            top: preferredSize.height * 0.1,
-            bottom: preferredSize.height * 0.3),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Text(AppStrings.personalTasks,
-                style: const AppTextStyles()
-                    .headingH2
-                    .copyWith(color: AppColors.kWhite)),
-            Text(AppStrings.taskDescription,
-                style: const AppTextStyles()
-                    .bodyTextLargeRegular
-                    .copyWith(color: AppColors.kWhite.withOpacity(0.3))),
-          ],
-        ),
-      ),
-    );
+    return SliverAppBar(
+        snap: true,
+        pinned: true,
+        floating: true,
+        expandedHeight: preferredSize.height,
+        leading: IconButton(
+            onPressed: () => Navigator.pop(context),
+            icon: SvgPicture.asset(
+              AppIcons.leftArrow,
+              color: AppColors.kWhite,
+              height: AppSize.s12,
+            )),
+        backgroundColor: AppColors.kBlack,
+        systemOverlayStyle: const SystemUiOverlayStyle(
+            statusBarColor: Colors.transparent,
+            statusBarBrightness: Brightness.light,
+            statusBarIconBrightness: Brightness.light),
+        flexibleSpace: FlexibleSpaceBar(
+          titlePadding: EdgeInsets.only(
+            left: size.width * 0.15,
+            bottom: preferredSize.height * 0.2,
+          ),
+          title: Text(AppStrings.personalTasks,
+              style: const AppTextStyles()
+                  .headingH2
+                  .copyWith(color: AppColors.kWhite)),
+          background: Padding(
+            padding: EdgeInsets.only(
+                top: preferredSize.height * 0.9, left: size.width * 0.2),
+            child: Text(
+              AppStrings.taskDescription,
+              style: const AppTextStyles()
+                  .bodyTextLargeRegular
+                  .copyWith(color: AppColors.kWhite.withOpacity(0.3)),
+            ),
+          ),
+        ));
   }
 }
