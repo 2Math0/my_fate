09f1df6ec4e5a1ce08597a1db14fc40e89676001
2Math0 <toto.mm50.tm@gmail.com>
2023-01-19 05:38:21 +0200

    add a task model

---
+++ lib/data/model/task_model.dart
@@ -0,0 +1,92 @@
+class TaskModel {
+  /// the [title] that the task hold like the name of the task
+  String? title;
+
+  /// [description] is [nullable] [not required field] which the user fill to
+  /// give more details about the task
+  String? description;
+
+  /// the [startTime] is the field that's responsible to schedule the task
+  /// with start time
+  /// * push notification alert is created
+  /// * the hour with same value of startTime will hold true value in date key
+  /// Note book hour structure is
+  /// ```dart
+  /// {
+  ///   hour:{
+  ///     date: boolean(booked(true) or not(false))
+  ///      }
+  /// }
+  /// ```
+  String? startTime;
+
+  /// [hours] represent number of hours needed to complete the task
+  String? hours;
+
+  /// the [date] which the task is assigned
+  /// the date is a required parameter for push notification
+  String? date;
+
+  /// [category] is the parameter that assign the task to specific category
+  /// it holds with it the unique color
+  String? category;
+
+  /// ## Definition
+  /// TaskModel is the model of tasks in the app it controls
+  /// Adding, Categorize, Edit, Delete, isDone Tasks in constant time
+  ///
+  /// ## Parameters
+  /// the `title` that the task hold like the name of the task [@title]
+  ///
+  /// [description] is [nullable] [not required field] which the user fill to
+  /// give more details about the task
+  ///
+  /// the [startTime] is the field that's responsible to schedule the task
+  /// with start time
+  /// * push notification alert is created
+  /// * the hour with same value of startTime will hold true value in date key
+  /// Note book hour structure is
+  /// ```dart
+  /// {
+  ///   hour:{
+  ///     date: boolean(booked(true) or not(false))
+  ///      }
+  /// }
+  /// ```
+  ///
+  /// [hours] represent number of hours needed to complete the task
+  ///
+  /// the [date] which the task is assigned
+  /// the date is a required parameter for push notification
+  ///
+  /// [category] is the parameter that assign the task to specific category
+  /// it holds with it the unique color
+
+  TaskModel(
+      {this.title,
+      this.description,
+      this.startTime,
+      this.hours,
+      this.date,
+      this.category});
+
+  TaskModel.fromJson(Map<String, dynamic> json) {
+    title = json["title"];
+    description = json["description"];
+    startTime = json["start time"];
+    hours = json["hours"];
+    date = json["date"];
+    category = json["category"];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data["title"] = title;
+    data["description"] = description;
+    data["start time"] = startTime;
+    data["hours"] = hours;
+    data["date"] = date;
+    data["category"] = category;
+    return data;
+  }
+}
