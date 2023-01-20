2398406ff68a0dba460bf0966471f1f3f8f2c161
2Math0 <toto.mm50.tm@gmail.com>
2023-01-20 11:28:52 +0200

    add task categories db

---
+++ lib/data/tasks_categories.dart
@@ -0,0 +1,7 @@
+import 'package:my_fate/presentation/resources/colors_manager.dart';
+
+Map<String, ColorShades> taskCategory = {
+  "personal": AppColors.yellow,
+  "work": AppColors.red,
+  "health": AppColors.blue,
+};
