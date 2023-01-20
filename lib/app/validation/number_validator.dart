eb2c15ee3979bf41ae91bbbcace251dce0c21700
2Math0 <toto.mm50.tm@gmail.com>
2023-01-20 11:43:35 +0200

    multiple messages in numeric validation

--- lib/app/validation/number_validator.dart
+++ lib/app/validation/number_validator.dart
@@ -2,13 +2,26 @@ import '../../presentation/resources/strings_manager.dart';
 import 'base_validator.dart';
 
 class NumberValidator extends BaseValidator {
+  bool _isNumeric = false;
+  bool _isNotEmpty = false;
+
   @override
   String getMessage() {
-    return '* ${AppStrings.numberValidationMessage}';
+    String message = '';
+    if (!_isNumeric) {
+      message = '* ${AppStrings.numberValidationMessage}';
+    }
+    if (!_isNotEmpty) {
+      message =
+          "$message${message.isNotEmpty ? '\n' : ''}* ${AppStrings.requiredFieldMessage}";
+    }
+    return message;
   }
 
   @override
   bool validate(String? value) {
-    return RegExp(r'\d').hasMatch(value!);
+    _isNumeric = RegExp(r'\d').hasMatch(value!);
+    _isNotEmpty = value.isNotEmpty;
+    return _isNumeric && _isNotEmpty;
   }
 }
